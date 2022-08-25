fb568b4d66dd9013147b65578930504002555049
Ahmadam37 <ahmadam37@hotmail.com>
2022-08-25 15:21:02 +0300

    init commit2

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SelectComponent } from './components/select/select.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    SelectComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    NavbarComponent
+  ]
+})
+export class SharedModule { }
