17a7e8359759ce20a2db339ae673071b1ecb8700
Ahmadam37 <ahmadam37@hotmail.com>
2022-08-28 23:20:35 +0300

    Setup Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
   ],
   imports: [
+  AppRoutingModule,
+  RouterModule,
     BrowserModule,
-    AppRoutingModule,
     SharedModule,
     ProductsModule
   ],
