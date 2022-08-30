69fbf819584ae3af4863a08affae30a003eec18a
Ahmadam37 <ahmadam37@hotmail.com>
2022-08-30 18:11:48 +0300

    Create spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SelectComponent } from './components/select/select.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SelectComponent
+    SelectComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
